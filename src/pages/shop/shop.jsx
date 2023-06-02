bf943deb22867254943016bffd816a11831fdba5
silpa12s <silpa12sss@gmail.com>
2023-06-02 16:56:18 +0530

    first commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PRODUCTS } from '../../products';
+import { Product } from './product';
+import './shop.css';
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+           <h1> Beauty Hub</h1>
+        </div>
+        <div className='products'>
+          {PRODUCTS.map((product)=>(
+            <Product data={product}/>
+          ))}
+        </div>
+    </div>
+  )
+};
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/p1.webp";
+import product2 from "./assets/p22.jpg";
+import product3 from "./assets/p3.webp";
+import product4 from "./assets/pp.jpg";
+import product5 from "./assets/p5.jpg";
+import product6 from "./assets/p1.webp";
+import product7 from "./assets/p7.jpg";
+import product8 from "./assets/p22.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Eyeshadow palette",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Lipstic",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Matte Lipstics Box",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Bridal makeup kit",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "MakeUp brushes set",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Eyeshadow palette",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Foundation",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Red lipstic",
+    price: 40.0,
+    productImage: product8,
+  },
+];
